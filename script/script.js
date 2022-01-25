b84204d5d8b2ca04ff18b148202b2b2ca073b27b
Sergei Shabanov <shabanov1989@gmail.com>
2022-01-25 19:50:51 +0300

    исправил замечания

--- script/script.js
+++ script/script.js
@@ -3,29 +3,29 @@ let editButton = container.querySelector('.profile__button');
 let popup = container.querySelector('.popup');
 let closeButton = popup.querySelector('.popup__close-btn')
 let saveButton = popup.querySelector('.popup__button');
+let formElement = container.querySelector('.popup__container');
+let nameInput = popup.querySelector('.popup__text_type_name');
+let jobInput = popup.querySelector('.popup__text_type_occupation');
+let nameNew = container.querySelector('.profile__input-name');
+let jobNew= container.querySelector('.profile__input-occupation');
 
 function popupOpen() {
   popup.classList.add('popup_opened')
+  nameInput.value = nameNew.textContent;
+  jobInput.value = jobNew.textContent;
 };
 
 function popupClose() {
   popup.classList.remove('popup_opened')
 };
 
-editButton.addEventListener('click', popupOpen);
-closeButton.addEventListener('click', popupClose);
-saveButton.addEventListener('click', popupClose);
-
-let formElement = container.querySelector('.popup__container');
-let nameInput = popup.querySelector('.popup__text_name');
-let jobInput = popup.querySelector('.popup__text_occupation');
-let nameNew = container.querySelector('.profile__input-name');
-let JobNew= container.querySelector('.profile__input-occupation');
-
 function formSubmitHandler (evt) {
   evt.preventDefault();
   nameNew.textContent = nameInput.value;
-  JobNew.textContent = jobInput.value;
+  jobNew.textContent = jobInput.value;
+  popupClose();
 }
 
+editButton.addEventListener('click', popupOpen);
+closeButton.addEventListener('click', popupClose);
 formElement.addEventListener('submit', formSubmitHandler);
